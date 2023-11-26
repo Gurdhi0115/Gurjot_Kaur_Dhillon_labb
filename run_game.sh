4f0e7edb354aa8b04543e3ca059780a02b157ea7
Gurjot Kaur <gurdhi0115@skola.goteborg.se>
2023-11-26 15:28:22 +0100

    Commiting run game

---
+++ run_game.sh
@@ -0,0 +1,20 @@
+$run_game.sh
+Gurjot Kaur Dhillon program
+Running game from /mnt/c/Users/jotka/OneDrive/Documents/UV-Project/Assignment/Gurjot_Kaur_Dhillon_labb
+Compiling…..
+Running…..
+Think of a number between 1 and 10
+I'm going to ask a few questions in order to guess the number.
+Please answer T for true, and F for false.
+
+Is the number less than or equal to 5?
+T
+Is the number less than or equal to 3?
+F
+Is the number less than or equal to 4?
+T
+You were thinking about 4 (took me 3 guesses)
+Done!
+Removing class files…….
+Guesser.java  GuessingGame.java
+
